a21388acbba3c7e61372b32e61c8cea9ad6f8a67
lidunlife <uchihalidun@gmail.com>
2024-05-27 16:50:25 +0500

    clear button

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const inputBox = document.getElementById(`input-box`)
 const listContainer = document.getElementById(`list-container`)
+let delBtn = document.getElementById(`del`)
+delBtn.classList.add('clearButton')
 
 function addTask() {
     if (inputBox.value === "") {
@@ -15,22 +17,29 @@ function addTask() {
     inputBox.value = '';
     savedata();
 }
+delBtn.addEventListener('click', () => {
+    while (listContainer.firstChild) {
+        listContainer.removeChild(listContainer.firstChild)
+    }
+    input.value = "";
+    savedata();
+})
 
 listContainer.addEventListener('click', function (e) {
-    if (e.target.tagName === 'LI'){
+    if (e.target.tagName === 'LI') {
         e.target.classList.toggle(`checked`)
         savedata();
     }
-    else if (e.target.tagName === "SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         savedata();
     }
 }, false);
-
 function savedata() {
-    localStorage.setItem(`data`,listContainer.innerHTML);
+    localStorage.setItem(`data`, listContainer.innerHTML);
 }
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem('data')
 }
 showTask();
+
